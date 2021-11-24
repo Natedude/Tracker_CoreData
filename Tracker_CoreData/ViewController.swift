281c6242d23818ef64fd2baeac8a777a5fb7cc35
Natedude <diggity123@gmail.com>
2021-11-24 00:14:22 -0800

    Initial Commit

---
+++ Tracker_CoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tracker_CoreData
+//
+//  Created by Nathan Hildum on 11/24/21.
+//  Copyright © 2021 Nathan Hildum. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
