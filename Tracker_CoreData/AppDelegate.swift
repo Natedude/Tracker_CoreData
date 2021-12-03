5be11b6d4782d7b82e9906ebeca4f1daa6e2fc28
Natedude <diggity123@gmail.com>
2021-12-02 21:48:35 -0800

    add nav controller and tableview - builds but doesn't work

--- Tracker_CoreData/AppDelegate.swift
+++ Tracker_CoreData/AppDelegate.swift
@@ -13,11 +13,16 @@ import CoreDataManager
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+	var window: UIWindow?
+	
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 		// Override point for customization after application launch.
 		
 		CoreDataManager.sharedInstance.setupWithModel("Tracker_CoreData")
-		
+//		window = UIWindow()
+//		window?.makeKeyAndVisible()
+//		window?.rootViewController = ViewController()
+//		
 		return true
 	}
 
--- Tracker_CoreData/ViewController.swift
+++ Tracker_CoreData/ViewController.swift
@@ -4,13 +4,9 @@
 //  Copyright © 2021 Nathan Hildum. All rights reserved.
 /*
 * Inspired by:
-//  CoreDataManager.swift - DemoArcade
-//  Created by Jonathan Rasmusson (Contractor) on 2020-03-03.
-//  Copyright © 2020 Rasmusson Software Consulting. All rights reserved.
-* AND
-//  CoreDataManager.swift
-//  Created by Manish Rathi on 02/10/14.
-//  Copyright (c) 2014 Manish Rathi. All rights reserved.
+* - Included CoreDataManager Cocoapod
+* - Example app in the CoreDataManager repo
+*   - https://github.com/taaviteska/CoreDataManager
 */
 
 import UIKit
@@ -22,17 +18,17 @@ class ViewController: UIViewController {
 //	var cdm: CoreDataManager
 	private let cdm = CoreDataManager.sharedInstance
 
-	@IBOutlet weak var textView: UITextView!
-	@IBOutlet weak var addEntryButton: UIButton!
+//	@IBOutlet weak var textView: UITextView!
+//	@IBOutlet weak var addEntryButton: UIButton!
 	let textViewStart = "Entries:\n"
 //	var viewDidLoad = false
 	let format = DateFormatter()
 	
-	@IBAction func addEntryButtonPress(_ sender: Any) {
-		print("addEntryButtonPress")
-		self.insertNewEntry(sender: self)
-		self.refreshEntries()
-	}
+//	@IBAction func addEntryButtonPress(_ sender: Any) {
+//		print("addEntryButtonPress")
+//		self.insertNewEntry(sender: self)
+////		self.refreshEntries()
+//	}
 	
 	var entries: [NSManagedObject] = []
 	// person.value(forKeyPath: "name") as? String
@@ -41,24 +37,7 @@ class ViewController: UIViewController {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
 		format.dateFormat = "h:mma"
-		
-//		let appDelegate = (UIApplication.shared.delegate as! AppDelegate)
-//		guard let appDelegate =
-//			UIApplication.shared.delegate as? AppDelegate else {
-//				return
-//		}
-//		let ctx: NSManagedObjectContext = appDelegate.persistentContainer.viewContext
-		
-//		let cdm = CoreDataManager.sharedInstance
-		
-		// Main context for UIKit
-//		let ctx = cdm.mainContext
-		
-		// Background context for making updates
-//		let bgctx = cdm.backgroundContext
-		
-		// load entries and show
-		self.refreshEntries()
+//		self.refreshEntries()
 //		self.deleteAllEntries()
 	}
 	
@@ -66,73 +45,65 @@ class ViewController: UIViewController {
 			return CoreDataManager.sharedInstance.mainContext
 	}
 	
-	func refreshEntries(){
-		let ctx = self.cdm.mainContext
-		self.entries = ctx.managerFor(Entry.self).array
-		self.displayEntries()
-	}
+//	func refreshEntries(){
+//		self.fetchEntries()
+//		self.displayEntries()
+//	}
 	
 	func deleteAllEntries(){
 		_ = self.cdm.mainContext.managerFor(Entry.self).delete()
 	}
 	
 	// Sync db -> self.entries
-	func fetchEntries(ctx: NSManagedObjectContext) {
-		// - get entries into array
-//		let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Entry")
-//		do {
-//			self.entries = try ctx.fetch(fetchRequest)
-//			print("fetchEntries:")
-//			self.printEntries()
-//		} catch let error as NSError {
-//			print("Could not fetch. \(error), \(error.userInfo)")
-//		}
+	func fetchEntries() {
+		let ctx = self.cdm.mainContext
 		self.entries = ctx.managerFor(Entry.self).array
 	}
 	
 
 	// display from self.entries to textView UI
-	func displayEntries(){
-		var text = ""
-//		textView.text = text
-		if self.entries.count == 0 {
-			text = "No entries"
-			print("displayEntries: No entries")
-		} else {
-			for e in self.entries {
-//				e = e as Entry
-				print(e)
-				let _id = e.value(forKey: "id") as? Int64
-				let _medName = e.value(forKeyPath: "medName") as? String
-				let _time = e.value(forKey: "time") as? Date
-				
-//				let medName = _medName
-//				let id = _id
-//				let time = _time
-			
-				guard let medName = _medName else {
-					print("displayEntries: ERROR: medName")
-					return
-				}
-				guard let id = _id else {
-					print("displayEntries: ERROR: id")
-					return
-				}
-				var timeStr = "No Date"
-				if _time  != nil {
-					let time = _time!
-					timeStr = format.string(from: time)
-				}
-				
-//				let fTime = format.string(from: time)
-//				print("displayEntries: medName: \(medName)")
-				text.append("id:\(id) - \(timeStr) - \(medName) \n")
-//				print("displayEntries: appended. text=\n\(text)")
-			}
-		}
-		self.textView.text = self.textViewStart + text
-	}
+//	func displayEntries(){
+//		var text = ""
+////		textView.text = text
+//		if self.entries.count == 0 {
+//			text = "No entries"
+//			print("displayEntries: No entries")
+//		} else {
+//			for e in self.entries {
+////				e = e as Entry
+//				print(e)
+//				let _id = e.value(forKey: "id") as? Int64
+//				let _medName = e.value(forKeyPath: "medName") as? String
+//				let _time = e.value(forKey: "time") as? Date
+//
+////				let medName = _medName
+////				let id = _id
+////				let time = _time
+//
+//				guard let medName = _medName else {
+//					print("displayEntries: ERROR: medName")
+//					return
+//				}
+//				guard let id = _id else {
+//					print("displayEntries: ERROR: id")
+//					return
+//				}
+//				var timeStr = "No Date"
+//				if _time  != nil {
+//					let time = _time!
+//					timeStr = format.string(from: time)
+//				}
+//
+////				let fTime = format.string(from: time)
+////				print("displayEntries: medName: \(medName)")
+//				text.append("id:\(id) - \(timeStr) - \(medName) \n")
+////				print("displayEntries: appended. text=\n\(text)")
+//			}
+//		}
+//		self.textView.text = self.textViewStart + text
+//	}
 	
+	// Create new row in Entry table
 	@objc func insertNewEntry(sender: AnyObject){
 		
 		let context = self.cdm.mainContext
@@ -155,28 +126,6 @@ class ViewController: UIViewController {
 //			return false
 		}
 	}
-//	// Create new row in Entry table
-//	func save(medName: String) {
-//		print("save: called")
-////		guard let appDelegate =
-////			UIApplication.shared.delegate as? AppDelegate else {
-////				return
-////		}
-//		let ctx = self.cdm.mainContext
-//
-//		let entity = NSEntityDescription.entity(forEntityName: "Entry",in: ctx)!
-//		let newEntry = NSManagedObject(entity: entity, insertInto: ctx)
-//		newEntry.setValue(medName, forKeyPath: "medName")
-//		print("save: newEntry = \(newEntry)")
-//		do {
-//			try ctx.save()
-//			self.entries.append(newEntry)
-//			print("save: appended. entries:")
-//			self.printEntries()
-//		} catch let error as NSError {
-//			print("Could not save. \(error), \(error.userInfo)")
-//		}
-//	}
 	
 	// log contents of self.entries
 	func printEntries(){
